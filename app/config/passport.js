57c2faba8c9686a9402ea51a668d7e6d333e5690
Mikhail Sinyakov <mikhail.sinyakov86@gmail.com>
2018-01-14 19:44:29 +0000

    Reafy for add functionality

--- app/config/passport.js
+++ app/config/passport.js
@@ -36,7 +36,6 @@ module.exports = passport => {
 					newUser.github.username = profile.username;
 					newUser.github.displayName = profile.displayName;
 					newUser.github.publicRepos = profile._json.public_repos;
-					newUser.nbrClicks.clicks = 0;
 
 					newUser.save(err => {
 						if (err) {
